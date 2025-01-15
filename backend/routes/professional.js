348a35da25ac2fc57850c83caa72c46cb8d03bb8
Sport97 <por21022@byui.edu>
2025-01-14 22:39:35 -0600

    only two files should be in master branch

--- backend/routes/professional.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-const professionalController = require("../controllers/professional");
-
-router.get("/", professionalController.buildBackend);
-
-module.exports = router;
