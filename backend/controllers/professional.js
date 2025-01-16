235caa029bae977a4f5046c76740f9e38a97f1fc
Sport97 <por21022@byui.edu>
2025-01-16 17:01:15 -0600

    included code before stretch

--- backend/controllers/professional.js
+++ backend/controllers/professional.js
@@ -1,5 +1,15 @@
 const mongodb = require("../db/connect");
 const profressionalController = {};
+const userData = require("../user.json");
+
+// profressionalController.buildBackend = async (req, res) => {
+//   try {
+//     return res.status(200).json(userData);
+//   } catch (error) {
+//     console.error("Error handling JSON data:", error);
+//     res.status(500).json({ error: "Failed to load user data" });
+//   }
+// };
 
 profressionalController.buildBackend = async (req, res) => {
   const result = await mongodb.getDb().db().collection("user").find();
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const env = require("dotenv").config();
 const port = process.env.PORT || 8080;
+const host = process.env.HOST;
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const mongodb = require("./db/connect");
@@ -15,11 +16,17 @@ app
   })
   .use("/professional", professionalRoutes);
 
+// app.listen(port, () => {
+//   console.log(`app listening on ${host}:${port}/professional`);
+// });
+
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
   } else {
     app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    console.log(
+      `Connected to Database and listening on ${host}:${port}/professional`
+    );
   }
 });
